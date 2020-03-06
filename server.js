46ec0207df4dd8a335df72d82f20825471bf97a6
Yash-Bhandari <y.bhandari1@share.epsb.ca>
2020-03-06 02:15:47 -0700

    simple socket

--- server.js
+++ server.js
@@ -8,9 +8,17 @@ const io = require('socket.io').listen(http);
 app.use(express.static(path.join(__dirname, 'build')));
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'build', 'index.html')));
 
+const boat_data = { driver: 'hello' };
+
 io.on('connection', socket => {
     console.log('a user connected');
-    socket.on('disconnect', () => console.log('user disconnected'));
+    socket.on('client_join', data => {
+        socket.join('clients');
+        socket.emit('boat_data', boat_data);
+    });
+    socket.on('boat_join', data => {
+        socket.join('boat');
+    })
 });
 
 http.listen('5000', () => console.log('listening on *:5000'));
